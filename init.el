836feb9432c7e077673559aa2729949bd5552406
yooz <oor.9@outlook.com>
2019-04-07 01:31:24 +0900

    add coding.el

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (add-to-list 'load-path "~/.emacs.d/inits")
 (load "looks")
 (load "myorg")
+(load "coding")
 
 ;; 文字コード
 (prefer-coding-system 'utf-8)
@@ -31,50 +32,28 @@
 ;; tab 
 (setq-default tab-width 4 indent-tabs-mode nil)
 
+;; auto-save
 (setq backup-inhibited t)
 (setq auto-save-default nil)
 (setq ring-bell-function 'ignore)
 
+;; auto-revert
+(global-auto-revert-mode t)
+
 (global-set-key "\C-r" 'execute-extended-command)
 (global-set-key "\C-l" 'avy-goto-word-0)
 
-(global-auto-revert-mode t)
-
 ;; packages
 (require 'swift-mode)
-(require 'multiple-cursors)
-(require 'smartrep)
 (require 'elscreen)
 (require 'tramp)
 (require 'web-mode)
 (require 'ivy)
-(require 'symbol-overlay)
 (require 'undo-tree)
-(require 'smartparens-config)
-(require 'volatile-highlights)
 (require 'neotree)
-(require 'company)
 (require 'google-this)
 (require 'howm)
 (require 'ace-jump-buffer)
-
-;; smartrep & multiple-cursors
-(declare-function smartrep-define-key "smartrep")
-(global-unset-key "\C-t")
-(smartrep-define-key global-map "C-t"
-    '(("C-t" . 'mc/mark-next-like-this)
-     ("n" . 'mc/mark-next-like-this)
-     ("p" . 'mc/mark-previous-like-this)
-     ("m" . 'mc/mark-more-like-this-extended)
-     ("u" . 'mc/unmark-next-like-this)
-     ("U" . 'mc/unmark-previous-like-this)
-     ("s" . 'mc/skip-to-next-like-this)
-     ("S" . 'mc/skip-to-previous-like-this)
-     ("*" . 'mc/mark-all-like-this)
-     ("d" . 'mc/mark-all-like-this-dwim)
-     ("i" . 'mc/insert-numbers)
-     ("o" . 'mc/sort-regions)
-     ("O" . 'mc/reverse-regions)))
 
 ;;Elscreen
 (elscreen-set-prefix-key "\C-z")
@@ -101,14 +80,6 @@
 (global-set-key "\C-s" 'swiper)
 (defvar swiper-include-line-number-in-search t)
 
-;; symbol-overlay
-(add-hook 'prog-mode-hook #'symbol-overlay-mode)
-(add-hook 'markdown-mode-hook #'symbol-overlay-mode)
-(global-set-key (kbd "M-i") 'symbol-overlay-put)
-(define-key symbol-overlay-map (kbd "p") 'symbol-overlay-jump-prev) 
-(define-key symbol-overlay-map (kbd "n") 'symbol-overlay-jump-next) 
-(define-key symbol-overlay-map (kbd "C-g") 'symbol-overlay-remove-all) 
-
 ;; google-this
 (with-eval-after-load "google-this"
     (defun my:google-this ()
@@ -118,39 +89,9 @@
 ;; undo-tree
 (global-undo-tree-mode t)
 
-;; smartparens
-(smartparens-global-mode t)
-
-;; volatile-highlights
-(volatile-highlights-mode t)
-
-;; dumb-jump 
-(setq dumb-jump-mode t)
-(setq dumb-jump-selector 'ivy)
-(setq dumb-jump-use-visible-window nil)
-
 ;; neotree
 (setq neo-theme 'icons)
 (setq neo-smart-open t)
-
-;; company
-(global-company-mode)
-(setq company-transformers '(company-sort-by-backend-importance))
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-lengh 2)
-(setq company-selection-wrap-around t)
-(setq completion-ignore-case t)
-(setq company-dabbrev-downcase nil)
-(global-set-key (kbd "C-M-i") 'company-complete)
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(define-key company-search-map (kbd "C-n") 'company-select-next)
-(define-key company-search-map (kbd "C-p") 'company-select-previous)
-(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-(define-key company-active-map (kbd "C-i") 'company-complete-selection)
-(define-key company-active-map [tab] 'company-complete-selection)
-(define-key company-active-map (kbd "C-f") 'company-complete-selection)
-(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
 
 ;; howm
 (setq howm-directory (concat user-emacs-directory "howm"))
---
+++ inits/coding.el
@@ -0,0 +1,62 @@
+;; smartrep & multiple-cursors
+(require 'multiple-cursors)
+(require 'smartrep)
+(declare-function smartrep-define-key "smartrep")
+(global-unset-key "\C-t")
+(smartrep-define-key global-map "C-t"
+  '(("C-t" . 'mc/mark-next-like-this)
+    ("n" . 'mc/mark-next-like-this)
+    ("p" . 'mc/mark-previous-like-this)
+    ("m" . 'mc/mark-more-like-this-extended)
+    ("u" . 'mc/unmark-next-like-this)
+    ("U" . 'mc/unmark-previous-like-this)
+    ("s" . 'mc/skip-to-next-like-this)
+    ("S" . 'mc/skip-to-previous-like-this)
+    ("*" . 'mc/mark-all-like-this)
+    ("d" . 'mc/mark-all-like-this-dwim)
+    ("i" . 'mc/insert-numbers)
+    ("o" . 'mc/sort-regions)
+    ("O" . 'mc/reverse-regions)))
+
+;; symbol-overlay
+(require 'symbol-overlay)
+(add-hook 'prog-mode-hook #'symbol-overlay-mode)
+(add-hook 'markdown-mode-hook #'symbol-overlay-mode)
+(global-set-key (kbd "M-i") 'symbol-overlay-put)
+(define-key symbol-overlay-map (kbd "p") 'symbol-overlay-jump-prev)
+(define-key symbol-overlay-map (kbd "n") 'symbol-overlay-jump-next)
+(define-key symbol-overlay-map (kbd "C-g") 'symbol-overlay-remove-all)
+
+;; smartparens
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+;; volatile-highlights
+(require 'volatile-highlights)
+(volatile-highlights-mode t)
+
+;; dumb-jump
+(require 'dumb-jump)
+(setq dumb-jump-mode t)
+(setq dumb-jump-selector 'ivy)
+(setq dumb-jump-use-visible-window nil)
+
+;; company
+(require 'company)
+(global-company-mode)
+(setq company-transformers '(company-sort-by-backend-importance))
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-lengh 2)
+(setq company-selection-wrap-around t)
+(setq completion-ignore-case t)
+(setq company-dabbrev-downcase nil)
+(global-set-key (kbd "C-M-i") 'company-complete)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+(define-key company-active-map (kbd "C-i") 'company-complete-selection)
+(define-key company-active-map [tab] 'company-complete-selection)
+(define-key company-active-map (kbd "C-f") 'company-complete-selection)
+(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
