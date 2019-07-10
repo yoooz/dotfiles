7f600e179b370edd89b713f3a69d273fdcd074bd
yooz-o <oor.9@outloock.com>
2019-07-10 15:10:40 +0900

    news feed

--- emacs.d/init/00_twitter.el
+++ emacs.d/init/00_twitter.el
@@ -2,3 +2,19 @@
   :config
   (setq twittering-icon-mode t)
   )
+
+(setq newsticker-url-list
+      '(
+        ("Qiita Emacs" "http://qiita.com/tags/Emacs/feed.atom"))) 
+
+
+(use-package elfeed
+  :config
+  (setq elfeed-feeds
+        '("http://qiita.com/tags/Emacs/feed.atom"
+          "http://qiita.com/tags/Kotlin/feed.atom"
+          "http://qiita.com/tags/Swift/feed.atom"
+          "http://qiita.com/tags/Terminal/feed.atom"
+          "http://qiita.com/tags/tmux/feed.atom")
+        )
+  )
