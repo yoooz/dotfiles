3d2de1ddcf61f3a38504854f426e232c15feadb8
yooz <oor.9@outlook.com>
2019-07-11 00:19:25 +0900

    newsticker add feeds

--- emacs.d/init/00_twitter.el
+++ emacs.d/init/00_twitter.el
@@ -5,16 +5,27 @@
 
 (setq newsticker-url-list
       '(
-        ("Qiita Emacs" "http://qiita.com/tags/Emacs/feed.atom"))) 
+        ("Qiita Emacs" "http://qiita.com/tags/Emacs/feed.atom")
+        ("Qiita Kotlin" "http://qiita.com/tags/Kotlin/feed.atom")
+        ("Qiita Swift" "http://qiita.com/tags/Swift/feed.atom")
+        ("Qiita Android" "http://qiita.com/tags/Android/feed.atom")
+        ("Qiita iOS" "http://qiita.com/tags/iOS/feed.atom")
+        ("Qiita tmux" "http://qiita.com/tags/tmux/feed.atom")
+        ("Qiita zsh" "http://qiita.com/tags/zsh/feed.atom")
+        ("Qiita elisp" "http://qiita.com/tags/elisp/feed.atom")
 
+        ("Github Trending emacs-lisp" "https://mshibanami.github.io/GitHubTrendingRSS/daily/emacs-lisp.xml")
+        ("Github Trending kotlin" "https://mshibanami.github.io/GitHubTrendingRSS/daily/kotlin.xml")
 
-(use-package elfeed
-  :config
-  (setq elfeed-feeds
-        '("http://qiita.com/tags/Emacs/feed.atom"
-          "http://qiita.com/tags/Kotlin/feed.atom"
-          "http://qiita.com/tags/Swift/feed.atom"
-          "http://qiita.com/tags/Terminal/feed.atom"
-          "http://qiita.com/tags/tmux/feed.atom")
+        ("Life Hacker" "http://feeds.lifehacker.jp/rss/lifehacker/index.xml")
+        ("GIGAZINE" "http://gigazine.net/news/rss_2.0/")
+        ("ビジネスジャーナル" "http://biz-journal.jp/index.xml")
+        ("朝日新聞デジタル" "http://www3.asahi.com/rss/index.rdf")
+        ("ギズモード・ジャパン" "http://www.gizmodo.jp/atom.xml")
+        ("techcrunch" "https://jp.techcrunch.com/feed/")
+        ("gori.me" "https://gori.me/feed")
+
+        ("planet ubuntu" "http://planet.ubuntu.com/rss20.xml")
+        ("ubuntu blog" "https://admin.insights.ubuntu.com/feed")
         )
-  )
+      )
