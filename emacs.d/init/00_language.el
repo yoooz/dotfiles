453097729ffbb9c4d3afa0f3e9c8b430648baaaf
yooz.o <oor.9@outlook.com>
2019-08-12 14:01:34 +0900

    lsp-mode

--- emacs.d/init/00_language.el
+++ emacs.d/init/00_language.el
@@ -1,3 +1,79 @@
+;; yasnippet
+(use-package yasnippet)
+
+;; Language Server Protocol
+(use-package lsp-mode)
+(use-package lsp-ui
+  :config
+  ;; lsp-ui-doc
+  (custom-set-variables
+   '(lsp-ui-doc-enable nil)
+   '(lsp-ui-doc-header t)
+   '(lsp-ui-doc-include-signature t)
+   '(lsp-ui-doc-position 'top)
+   '(lsp-ui-doc-alignment 'frame)
+   '(lsp-ui-doc-border "blue")
+   '(lsp-ui-doc-use-childframe t)
+   '(lsp-ui-doc-use-webkit t)   
+   '(lsp-ui-doc-delay 0.1)
+   )
+  (custom-set-faces
+   '(lsp-ui-doc-background ((t (:background "black"))))
+   )
+  ;; lsp-ui-flycheck
+  (custom-set-variables
+   '(lsp-ui-flycheck-enable nil)
+   )
+  ;; lsp-ui-sideline
+  (custom-set-variables
+   '(lsp-ui-sideline-enable t)
+   '(lsp-ui-sideline-ignore-duplicate t)
+   '(lsp-ui-sideline-show-symbol t)
+   '(lsp-ui-sideline-show-hover t)
+   '(lsp-ui-sideline-show-diagnostics t)
+   '(lsp-ui-sideline-show-code-actions t)
+   '(lsp-ui-sideline-update-mode 'line)
+   '(lsp-ui-sideline-delay 0.1)
+   )
+  ;; lsp-ui-imenu
+  ;; lsp-ui-peek
+  (custom-set-variables
+   '(lsp-ui-peek-enable t)
+   '(lsp-ui-peek-always-show t)
+   )
+
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  )
+
+(use-package company-lsp)
+
+;; company
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-transformers '(company-sort-by-backend-importance))
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-lengh 2)
+  (setq company-selection-wrap-around t)
+  (setq completion-ignore-case t)
+  (setq company-dabbrev-downcase nil)
+  (global-set-key (kbd "C-M-i") 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+  (define-key company-active-map (kbd "C-i") 'company-complete-selection)
+  (define-key company-active-map [tab] 'company-complete-selection)
+  (define-key company-active-map (kbd "C-f") 'company-complete-selection)
+  (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+  (push 'company-lsp company-backends)
+  )
+
+(use-package python-mode
+  :config
+  (add-hook 'python-mode-hook #'lsp))
+
 ;; kotlin
 (use-package kotlin-mode)
 
