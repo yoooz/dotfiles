297d29f9869058eaa4fb57918313b0416c3e3c8e
yoooz <boc.7zyzy@gmail.com>
2019-08-17 00:43:10 +0900

    30_util.el custom

--- emacs.d/init/30_util.el
+++ emacs.d/init/30_util.el
@@ -1,7 +1,7 @@
 ;; tramp
 (use-package tramp
-  :config
-  (setq tramp-default-method "ssh")
+  :custom
+  (tramp-default-method "ssh")
   )
 
 ;; google-this
@@ -11,12 +11,11 @@
 (use-package avy
   :config
   (setq avy-background t)
-  (custom-set-faces
-   '(avy-lead-face ((t (:foreground "red" :background "black"))))
-   '(avy-lead-face-0 ((t (:foreground "green" :background "black"))))
-   '(avy-lead-face-1 ((t (:foreground "yellow" :background "black"))))
-   '(avy-lead-face-2 ((t (:foreground "blue" :background "black"))))
-   )
+  :custom-face
+  (avy-lead-face ((t (:foreground "red" :background "black"))))
+  (avy-lead-face-0 ((t (:foreground "green" :background "black"))))
+  (avy-lead-face-1 ((t (:foreground "yellow" :background "black"))))
+  (avy-lead-face-2 ((t (:foreground "blue" :background "black"))))
   :bind
   (
    ("C-l" . avy-goto-word-1))
@@ -24,10 +23,8 @@
 
 ;; ace-window
 (use-package ace-window
-  :config
-  (custom-set-variables
-   '(aw-keys '(?j ?k ?l ?i ?o ?h ?y ?u ?p))
-   )
+  :custom
+  (aw-keys '(?j ?k ?l ?i ?o ?h ?y ?u ?p))
   :custom-face
   (aw-leading-char-face ((t (:height 4.0 :foreground "#f1fa8c"))))
 )
@@ -40,14 +37,13 @@
   (setq enable-recursive-minibuffers t)
   (setq ivy-re-builders-alist
         '((t . ivy--regex-plus)))
-  (custom-set-variables
-   '(swiper-include-line-number-in-search t)
-   '(ivy-use-virtual-buffers nil)
-   '(ivy-height 20)
-   '(ivy-extra-directories nil)
-   '(ivy-format-functions-alist '((t . ivy-format-function-arrow)))
-   '(ivy-initial-inputs-alist '((counsel-M-x . "")))
-   )
+  :custom
+  (swiper-include-line-number-in-search t)
+  (ivy-use-virtual-buffers nil)
+  (ivy-height 20)
+  (ivy-extra-directories nil)
+  (ivy-format-functions-alist '((t . ivy-format-function-arrow)))
+  (ivy-initial-inputs-alist '((counsel-M-x . "")))
   :bind
   ("C-s" . swiper)
   )
@@ -66,21 +62,18 @@
 ;; ivy-posframe (GUI Emacs Only)
 (use-package ivy-posframe
   :diminish ivy-posframe-mode
-  :config
-  (custom-set-faces
-   '(ivy-posframe-border((t (:background "CornflowerBlue"))))
-   )
-  (custom-set-variables
-   '(ivy-posframe-display-functions-alist
-     '((swiper . nil)
-       (t . ivy-posframe-display-at-frame-top-center)
-       )
+  :custom-face
+  (ivy-posframe-border((t (:background "CornflowerBlue"))))
+  :custom
+  (ivy-posframe-display-functions-alist
+   '((swiper . nil)
+     (t . ivy-posframe-display-at-frame-top-center)
      )
-   '(ivy-posframe-mode 1)
-   '(ivy-posframe-parameters
-        '((left-fringe . 8)
-          (right-fringe . 8)))
    )
+  (ivy-posframe-mode 1)
+  (ivy-posframe-parameters
+   '((left-fringe . 8)
+     (right-fringe . 8)))
   )
 
 ;; all the icons ivy 
@@ -93,16 +86,15 @@
   (ivy-rich-mode -1))
 
 (use-package migemo
-  :config
-  (setq migemo-command "cmigemo")
-  (setq migemo-options '("-q" "--emacs"))
- 
+  :custom
+  (migemo-command "cmigemo")
+  (migemo-options '("-q" "--emacs"))
   ;; Set your installed path
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-  
-  (setq migemo-user-dictionary nil)
-  (setq migemo-regex-dictionary nil)
-  (setq migemo-coding-system 'utf-8-unix)
+  (migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (migemo-user-dictionary nil)
+  (migemo-regex-dictionary nil)
+  (migemo-coding-system 'utf-8-unix)
+  :config
   (migemo-init)
   )
 
@@ -120,24 +112,22 @@
   :diminish beacon-mode
   :config
   (beacon-mode t)
-  (custom-set-variables
-   '(beacon-size 10)
-   '(beacon-color 0.2)
-   '(beacon-blink-delay 0.1)
-   '(beacon-blink-when-focused t))
+  :custom
+  (beacon-size 10)
+  (beacon-color 0.2)
+  (beacon-blink-delay 0.1)
+  (beacon-blink-when-focused t)
   )
 
 (use-package zoom
   :diminish zoom-mode
+  :custom
+  (zoom-size '(0.618 . 0.618))
   :config
-  (custom-set-variables
-   '(zoom-size '(0.618 . 0.618))
-   '(zoom-mode t)
-   )
+  (zoom-mode t)
   )
 
 (use-package hungry-delete
   :diminish hungry-delete-mode
-  :hook
-  ((prog-mode) . hungry-delete-mode)
+  :hook (prog-mode . hungry-delete-mode)
   )
