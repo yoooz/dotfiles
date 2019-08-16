e438a772539a6054e472d88a7773c4490605f92d
yoooz <boc.7zyzy@gmail.com>
2019-08-17 00:55:52 +0900

    20_language.el custom

--- emacs.d/init/20_language.el
+++ emacs.d/init/20_language.el
@@ -17,45 +17,35 @@
 ;; Language Server Protocol
 (use-package lsp-mode)
 (use-package lsp-ui
-  :config
+  :custom
   ;; lsp-ui-doc
-  (custom-set-variables
-   '(lsp-ui-doc-enable t)
-   '(lsp-ui-doc-header t)
-   '(lsp-ui-doc-include-signature t)
-   '(lsp-ui-doc-position 'top)
-   '(lsp-ui-doc-alignment 'frame)
-   '(lsp-ui-doc-border "SteelBlue")
-   '(lsp-ui-doc-use-childframe t)
-   '(lsp-ui-doc-use-webkit t)   
-   '(lsp-ui-doc-delay 0.1)
-   )
-  (custom-set-faces
-   '(lsp-ui-doc-background ((t (:background "#282a36"))))
-   )
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-header t)
+  (lsp-ui-doc-include-signature t)
+  (lsp-ui-doc-position 'top)
+  (lsp-ui-doc-alignment 'frame)
+  (lsp-ui-doc-border "SteelBlue")
+  (lsp-ui-doc-use-childframe t)
+  (lsp-ui-doc-use-webkit t)   
+  (lsp-ui-doc-delay 0.1)
   ;; lsp-ui-flycheck
-  (custom-set-variables
-   '(lsp-ui-flycheck-enable nil)
-   )
+  (lsp-ui-flycheck-enable nil)
   ;; lsp-ui-sideline
-  (custom-set-variables
-   '(lsp-ui-sideline-enable t)
-   '(lsp-ui-sideline-ignore-duplicate t)
-   '(lsp-ui-sideline-show-symbol t)
-   '(lsp-ui-sideline-show-hover t)
-   '(lsp-ui-sideline-show-diagnostics t)
-   '(lsp-ui-sideline-show-code-actions t)
-   '(lsp-ui-sideline-update-mode 'line)
-   '(lsp-ui-sideline-delay 0.1)
-   )
+  (lsp-ui-sideline-enable t)
+  (lsp-ui-sideline-ignore-duplicate t)
+  (lsp-ui-sideline-show-symbol t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-sideline-show-diagnostics t)
+  (lsp-ui-sideline-show-code-actions t)
+  (lsp-ui-sideline-update-mode 'line)
+  (lsp-ui-sideline-delay 0.1)
   ;; lsp-ui-imenu
   ;; lsp-ui-peek
-  (custom-set-variables
-   '(lsp-ui-peek-enable t)
-   '(lsp-ui-peek-always-show t)
-   )
-
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  (lsp-ui-peek-enable t)
+  (lsp-ui-peek-always-show t)
+  :custom-face
+  (lsp-ui-doc-background ((t (:background "#282a36"))))
+  :hook (lsp-mode . lsp-ui-mode)
   )
 
 (use-package company-lsp)
@@ -65,28 +55,34 @@
   :diminish company-mode
   :config
   (global-company-mode)
-  (setq company-transformers '(company-sort-by-backend-importance))
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-lengh 2)
-  (setq company-selection-wrap-around t)
   (setq completion-ignore-case t)
-  (setq company-dabbrev-downcase nil)
-  (global-set-key (kbd "C-M-i") 'company-complete)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-search-map (kbd "C-n") 'company-select-next)
-  (define-key company-search-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-  (define-key company-active-map (kbd "C-i") 'company-complete-selection)
-  (define-key company-active-map [tab] 'company-complete-selection)
-  (define-key company-active-map (kbd "C-f") 'company-complete-selection)
-  (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
   (push 'company-lsp company-backends)
+  :custom 
+  (company-transformers '(company-sort-by-backend-importance))
+  (company-idle-delay 0)
+  (company-minimum-prefix-lengh 2)
+  (company-selection-wrap-around t)
+  (company-dabbrev-downcase nil)
+  :bind
+  (("C-M-i" . company-complete)
+   :map company-active-map
+   ("C-n" . company-select-next)
+   ("C-p" . company-select-previous)
+   ("C-s" . company-filter-candidates)
+   ("C-i" . company-complete-selection)
+   ([tab] . company-complete-selection)
+   ("C-f" . company-complete-selection)
+   :map company-search-map
+   ("C-n" . company-select-next)
+   ("C-p" . company-select-previous)
+   :map emacs-lisp-mode-map
+   ("C-M-i" . company-complete)
+   )
   )
 
 (use-package python-mode
-  :config
-  (add-hook 'python-mode-hook #'lsp))
+  :hook (python-mode . lsp-mode)
+  )
 
 ;; kotlin
 (use-package kotlin-mode)
