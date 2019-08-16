da5bc61497c148a8dc27a1f26bfa1ec166428587
yoooz <boc.7zyzy@gmail.com>
2019-08-17 01:05:14 +0900

    50_howm.el custom

--- emacs.d/init/50_howm.el
+++ emacs.d/init/50_howm.el
@@ -1,13 +1,14 @@
 ;; howm
 (use-package howm
+  :custom
+  (howm-file-name-format "%Y/%m/%Y_%m_%d.txt")
+  (howm-menu-lang 'ja)
+  (howm-keyword-case-fold-search t)
   :config
-  (setq howm-file-name-format "%Y/%m/%Y_%m_%d.txt")
   (setq howm-keyword-file "~/howm/.howm-keys")
-  (setq howm-menu-lang 'ja)
   (setq action-lock-no-browser t)
   (defvar howm-view-title-header "# ")
   (defvar datetime-format "%Y-%m-%dT%H:%M:%S")
   (setq howm-template-date-format
         (concat "[" datetime-format "]"))
-  (setq howm-keyword-case-fold-search t)
   )
