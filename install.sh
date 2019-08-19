5f9223a92801ac2cce11244e7e3f1a36854ea6f0
yoooz <boc.7zyzy@gmail.com>
2019-08-19 14:38:34 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-echo ${SCRIPT_DIR}/emacs.d
-echo ${SCRIPT_DIR}/zshrc
-echo ${SCRIPT_DIR}/tmux.conf
+ln -s ${SCRIPT_DIR}/emacs.d ${HOME}/.emacs.d
+ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
+ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
