ca0eb62026e9df12e86552402b595a66e3eb995c
yoooz <boc.7zyzy@gmail.com>
2020-01-03 23:47:38 +0900

    evil

--- emacs.d/init/70_sandbox.el
+++ emacs.d/init/70_sandbox.el
@@ -2,5 +2,10 @@
   :hook (kotlin-mode . electric-case-java-init)
   )
 
-;; evil-mode
-;(use-package evil)
+;(use-package evil
+;  :config
+;  (evil-mode t)
+;  )
+;
+;(defalias 'evil-insert-state 'evil-emacs-state)
+;(define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
