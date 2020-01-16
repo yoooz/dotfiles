568d28dc5575ec418583ea63403afc8e253fad49
yoooz <oor.9@outlook.com>
2020-01-16 15:00:37 +0900

    edit install.sh to add ideavimrc

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -s ${SCRIPT_DIR}/emacs.d ${HOME}/.emacs.d
 ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
 ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${SCRIPT_DIR}/peco ${HOME}/.config/peco
+ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
