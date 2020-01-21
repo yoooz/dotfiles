a6b2930fd58b06e6d2fd445e72982fef418a70a1
yoooz <oor.9@outlook.com>
2020-01-21 12:36:31 +0900

    create vimrc

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
 ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${SCRIPT_DIR}/peco ${HOME}/.config/peco
 ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
+ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
