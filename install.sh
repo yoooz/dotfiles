d7de6e098b6b3ec67c046d8dc8724aff7a39d5aa
yoooz <oor.9@outlook.com>
2020-01-28 14:08:07 +0900

    [add] tmux-pane-border

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${SCRIPT_DIR}/peco ${HOME}/.config/peco
 ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
 ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
+ln -s ${SCRIPT_DIR}/tmux-pane-border ${HOME}/bin/tmux-pane-border
