cdb5c64c47da7d85aa79189fae65ab8bb4f34e64
yoooz <oor.9@outlook.com>
2021-01-03 04:29:49 +0900

    [emacs] custom git-gutter face

--- emacs.d/init/20_coding.el
+++ emacs.d/init/20_coding.el
@@ -38,6 +38,14 @@
   :config
   (global-git-gutter-mode t)
   (setq git-gutter:update-hooks '(after-save-hook after-revert-hook))
+  :custom
+  (git-gutter:modified-sign " ")
+  (git-gutter:added-sign " ")
+  (git-gutter:deleted-sign " ")
+  :custom-face
+  (git-gutter:modified ((t (:background "#f1fa8c"))))
+  (git-gutter:added ((t (:background "#50fa7b"))))
+  (git-gutter:deleted ((t (:background "#ff79c6"))))
   )
 
 (use-package which-key
