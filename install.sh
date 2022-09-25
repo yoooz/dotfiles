a962e7532de1128005b0106be0d15696461c3c30
yoooz <oor.9@outlook.com>
2022-09-25 23:13:45 +0900

    Add Lazygit config

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ ln -s ${SCRIPT_DIR}/peco ${HOME}/.config/peco
 ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
 ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
 ln -s ${SCRIPT_DIR}/tmux-pane-border ${HOME}/bin/tmux-pane-border
+ln -s ${SCRIPT_DIR}/lazygit_config.yml ${HOME}/Library/Application\ Support/lazygit/config.yml
