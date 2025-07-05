fcd26f398c7543d5f23bd3ae4a7b50ce7f15792a
yoooz <boc.7zyzy@gmail.com>
2025-07-05 12:26:35 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(
+  cd $(dirname $0)
+  pwd
+)
 
-ln -s ${SCRIPT_DIR}/emacs.d ${HOME}/.emacs.d
 ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
 ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${SCRIPT_DIR}/peco ${HOME}/.config/peco
