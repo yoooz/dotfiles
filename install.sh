5ae1e9163c101bd313c589ce1d9d5a314348c74e
yoooz <boc.7zyzy@gmail.com>
2025-07-05 14:15:18 +0900

    replace tmux-powerline

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ SCRIPT_DIR=$(
 
 ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
 ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
-ln -s ${SCRIPT_DIR}/peco ${HOME}/.config/peco
+ln -s ${SCRIPT_DIR}/tmux-powerline ${HOME}/.config/tmux-powerline
 ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
 ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
-ln -s ${SCRIPT_DIR}/tmux-pane-border ${HOME}/bin/tmux-pane-border
 ln -s ${SCRIPT_DIR}/lazygit_config.yml ${HOME}/Library/Application\ Support/lazygit/config.yml
--- tmux-powerline/lib/arg_processing.sh
+++
@@ -1,9 +0,0 @@
-#! Check script arguments.
-
-check_arg_side() {
-	local side="$1"		
-	if ! [ "$side" ==  "left" -o "$side" == "right" -o "$side" == "pane" ]; then
-		echo "Argument must be the side to handle {left, right, pane} and not \"${side}\"."
-    	exit 1
-	fi
-}
