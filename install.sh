8771cdab45048f4b3388eec20245e23f08c974e5
yoooz <boc.7zyzy@gmail.com>
2025-07-05 14:18:12 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ SCRIPT_DIR=$(
 ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
 ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${SCRIPT_DIR}/tmux-powerline ${HOME}/.config/tmux-powerline
+ln -s ${SCRIPT_DIR}/nvim ${HOME}/.config/nvim
 ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
 ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
 ln -s ${SCRIPT_DIR}/lazygit_config.yml ${HOME}/Library/Application\ Support/lazygit/config.yml
