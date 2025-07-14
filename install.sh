103382372bd0ed89c5ef356120bc333f5fd089cf
yoooz <boc.7zyzy@gmail.com>
2025-07-14 23:08:44 +0900

    add claude settings

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ ln -s ${SCRIPT_DIR}/nvim ${HOME}/.config/nvim
 ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
 ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
 ln -s ${SCRIPT_DIR}/lazygit_config.yml ${HOME}/Library/Application\ Support/lazygit/config.yml
+ln -s ${SCRIPT_DIR}/claude/CLAUDE.md ~/.claude/CLAUDE.md
+ln -s ${SCRIPT_DIR}/claude/frieren.md ~/.claude/frieren.md
