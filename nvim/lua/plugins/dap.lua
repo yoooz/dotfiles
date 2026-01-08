04c292745bddeda7fb3184aac78f3364bd60801e
yoooz <boc.7zyzy@gmail.com>
2026-01-08 11:16:08 +0900

    install nvim-dap-ui

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -10,6 +10,27 @@ return {
         "leoluz/nvim-dap-go",
         opts = {},
       },
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        opts = {},
+        config = function(_, opts)
+          local dap = require("dap")
+          local dapui = require("dapui")
+          dapui.setup(opts)
+          -- デバッグ開始時に自動でUIを開く
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open()
+          end
+          -- デバッグ終了時に自動でUIを閉じる
+          dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close()
+          end
+        end,
+      },
     },
     keys = {
       {
@@ -19,6 +40,13 @@ return {
         end,
         desc = "Debug Nearest",
       },
+      { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
     },
   },
 }
