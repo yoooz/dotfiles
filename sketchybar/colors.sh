77acdf3bd639ffce64069b335dabe3606b2b1821
yoooz <boc.7zyzy@gmail.com>
2026-01-17 21:38:34 +0900

    fix sketchybar

---
+++ sketchybar/colors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff1e1e2e
+export WHITE=0xffcdd6f4
+export RED=0xfff38ba8
+export GREEN=0xffa6e3a1
+export BLUE=0xff89b4fa
+export YELLOW=0xfff9e2af
+export ORANGE=0xfffab387
+export MAGENTA=0xffcba6f7
+export GREY=0xff6c7086
+export TRANSPARENT=0x00000000
+
+# Bar colors
+export BAR_COLOR=0xf01e1e2e
+export BAR_BORDER_COLOR=0xff45475a
+
+# Item colors
+export ICON_COLOR=$WHITE
+export LABEL_COLOR=$WHITE
+export BACKGROUND_1=0xff313244
+export BACKGROUND_2=0xff45475a
+
+# Highlight colors
+export HIGHLIGHT=0xff89b4fa
+export HIGHLIGHT_WARN=0xfff9e2af
+export HIGHLIGHT_DANGER=0xfff38ba8
