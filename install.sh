23aee6c311ccb4455a294ced4324912f3cbef8a1
yoooz <boc.7zyzy@gmail.com>
2026-01-17 22:49:57 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ SCRIPT_DIR=$(
   pwd
 )
 
-ln -s ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
-ln -s ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
-ln -s ${SCRIPT_DIR}/tmux-powerline ${HOME}/.config/tmux-powerline
-ln -s ${SCRIPT_DIR}/nvim ${HOME}/.config/nvim
-ln -s ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
-ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
-ln -s ${SCRIPT_DIR}/lazygit_config.yml ${HOME}/Library/Application\ Support/lazygit/config.yml
-ln -s ${SCRIPT_DIR}/claude/CLAUDE.md ~/.claude/CLAUDE.md
-ln -s ${SCRIPT_DIR}/claude/scripts ~/.claude/scripts
-ln -s ${SCRIPT_DIR}/claude/settings.json ~/.claude/settings.json
-ln -s ${SCRIPT_DIR}/gitignore_global ~/.config/git/ignore
-ln -s ${SCRIPT_DIR}/ghostty ~/.config/ghostty
+ln -sf ${SCRIPT_DIR}/zshrc ${HOME}/.zshrc
+ln -sf ${SCRIPT_DIR}/tmux.conf ${HOME}/.tmux.conf
+ln -sf ${SCRIPT_DIR}/tmux-powerline ${HOME}/.config/tmux-powerline
+ln -sf ${SCRIPT_DIR}/nvim ${HOME}/.config/nvim
+ln -sf ${SCRIPT_DIR}/ideavimrc ${HOME}/.ideavimrc
+ln -sf ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
+ln -sf ${SCRIPT_DIR}/lazygit_config.yml ${HOME}/Library/Application\ Support/lazygit/config.yml
+ln -sf ${SCRIPT_DIR}/claude/CLAUDE.md ~/.claude/CLAUDE.md
+ln -sf ${SCRIPT_DIR}/claude/scripts ~/.claude/scripts
+ln -sf ${SCRIPT_DIR}/claude/settings.json ~/.claude/settings.json
+ln -sf ${SCRIPT_DIR}/gitignore_global ~/.config/git/ignore
+ln -sf ${SCRIPT_DIR}/ghostty ~/.config/ghostty
+ln -sf ${SCRIPT_DIR}/sketchybar ~/.config/sketchybar
+ln -sf ${SCRIPT_DIR}/aerospace ~/.config/aerospace
